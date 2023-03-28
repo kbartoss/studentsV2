9252463a3e1448a215ddbfe6669f3824d35ab9d2
Kuba Bartoszewicz <76003954+kbartoss@users.noreply.github.com>
2023-03-28 23:01:58 +0200

    Delete Overlay.tsx

--- src/components/Overlay.tsx
+++
@@ -1,9 +0,0 @@
-import { StyledOverlay } from "../styles/Overlay.styles"
-
-const Overlay = ({ closeModal }:any) => {
-  return (
-    <StyledOverlay onClick={closeModal}></StyledOverlay>
-  )
-}
-
-export default Overlay
